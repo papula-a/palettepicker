40d22f2f3c961145c963d044740e99c3d7a87d1e
Diya Mustafa <129324508+deyah3@users.noreply.github.com>
2024-10-27 23:54:36 +0300

    layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,16 +12,16 @@ const fredoka = Fredoka({
   display: "swap",
 });
 
-// const geistSans = localFont({
-//   src: "../fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "../fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata = {
   title: "Create Next App",
