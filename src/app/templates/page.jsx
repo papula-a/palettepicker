1f1e5f9511d6cad7eaf8fc6dea436064bc27e269
papula-a <pa20381@auis.edu.krd>
2024-10-28 20:11:02 +0300

    Font changed to Fredoka

--- src/app/templates/page.jsx
+++ src/app/templates/page.jsx
@@ -5,25 +5,25 @@ const Templates = () => {
     <div>
       <div className="flex items-center justify-center w-full h-64 bg-gradient-to-r from-[#3F1B62] via-[#A881A7] to-pink-200">
         <div className="text-center text-lg text-white">
-          <h1 className="text-2xl m-3">OUR PRE-MADE TEMPLATES</h1>
+          <h1 className="text-2xl font-bold m-3">OUR PRE-MADE TEMPLATES</h1>
           <p className="text-lg m-3">
             Try your color combinations on{" "}
-            <span className="text-yellow-500">ready-to-use</span> templates
+            <span className="text-yellow-500 font-semibold">ready-to-use</span>{" "}
+            templates
           </p>
         </div>
       </div>
 
       <div className="min-h-screen py-10">
         <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* Render each Card with unique content */}
             <Card
               title="E-COMMERCE WEBSITE"
               description="Facilitate buying and selling goods and services online with this responsive E-commerce template."
               imageSrc="/static/ecommerce.png"
               altText="E-commerce"
-               linkHref="/e-commerce"
+              linkHref="/e-commerce"
             />
             <Card
               title="BLOG WEBSITE"
@@ -32,14 +32,13 @@ const Templates = () => {
               altText="Blog"
               linkHref="/blog"
             />
-            
             <Card
               title="PORTFOLIO WEBSITE"
               description="A sleek, modern template for displaying personal portfolios or creative works."
               imageSrc="/static/portfolio.png"
               altText="Portfolio"
               linkHref="/portfolio"
-            /> 
+            />
             <Card
               title="LANDING PAGE"
               description="This template is designed for single-page websites and product launches."
