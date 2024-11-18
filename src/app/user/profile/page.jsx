ef45a2d490a5757512af3965d43d67765d83fb3a
papula-a <pa20381@auis.edu.krd>
2024-11-18 16:38:26 +0300

    Changed profile page back to server page

--- src/app/user/profile/page.jsx
+++ src/app/user/profile/page.jsx
@@ -1,65 +1,30 @@
-"use client";
-
 import { deletePalette } from "@/actions/palette";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 import { FaUser } from "react-icons/fa";
 
-export default function Profile() {
-  const [user, setUser] = useState(null);
-  const [palettes, setPalettes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const router = useRouter();
+export const dynamic = "force-dynamic";
 
-  useEffect(() => {
-    async function fetchUserData() {
-      try {
-        const response = await fetch("/api/user");
+export default async function ProfilePage() {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-
-        const data = await response.json();
-
-        if (!data.user) {
-          throw new Error("No user data found");
-        }
-
-        setUser(data.user);
-        setPalettes(data.palettes || []);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-        setError(error.message);
-        router.push("/"); // More reliable client-side redirect
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchUserData();
-  }, [router]);
-
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        Loading...
-      </div>
-    );
+  if (!isUserAuthenticated) {
+    redirect("/api/auth/login");
   }
 
-  if (error) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen text-red-500">
-        <p>Error: {error}</p>
-        <Button onClick={() => router.push("/")}>Go to Home</Button>
-      </div>
-    );
+  const user = await getUser();
+
+  if (!user) {
+    redirect("/api/auth/login");
   }
+
+  const palettes = await prisma.palette.findMany({
+    where: { userId: user.id },
+  });
 
   return (
     <div className="min-h-screen flex flex-col py-6 p-6">
@@ -67,15 +32,15 @@ export default function Profile() {
         <Card className="w-full max-w-6xl p-4 mb-8 bg-white shadow-lg bg-opacity-70">
           <CardHeader className="flex items-center justify-center">
             <Avatar className="w-40 h-40 mb-4">
-              <AvatarImage src={user?.picture} alt={user?.given_name} />
+              <AvatarImage src={user.picture} alt={user.given_name} />
               <AvatarFallback>
                 <FaUser className="text-6xl" />
               </AvatarFallback>
             </Avatar>
             <h2 className="text-xl font-semibold">
-              {user?.given_name} {user?.family_name}
+              {user.given_name} {user.family_name}
             </h2>
-            <p className="text-gray-600">Email: {user?.email}</p>
+            <p className="text-gray-600">Email: {user.email}</p>
           </CardHeader>
         </Card>
         <Card className="w-full max-w-6xl bg-white shadow-lg bg-opacity-70">
@@ -105,18 +70,12 @@ export default function Profile() {
                   <span className="text-xl text-gray-700 font-medium min-w-[200px] text-center">
                     {palette.name}
                   </span>
-                  <div className="flex space-x-2">
-                    <form action={deletePalette}>
-                      <input type="hidden" name="id" value={palette.id} />
-                      <Button
-                        type="submit"
-                        variant="link"
-                        className="text-red-600 px-1"
-                      >
-                        Delete
-                      </Button>
-                    </form>
-                  </div>
+                  <form action={deletePalette}>
+                    <input type="hidden" name="id" value={palette.id} />
+                    <button type="submit" className="text-red-600 px-1">
+                      Delete
+                    </button>
+                  </form>
                 </div>
               ))
             ) : (
