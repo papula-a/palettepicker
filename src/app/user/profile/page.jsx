69b571b273df7578f9ed7a723c1128b5f41ab34e
papula-a <pa20381@auis.edu.krd>
2024-11-18 16:57:36 +0300

    Divided profile over different components

--- src/app/user/profile/page.jsx
+++ src/app/user/profile/page.jsx
@@ -1,10 +1,8 @@
-import { deletePalette } from "@/actions/palette";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { UserPalettesList } from "@/components/UserPalettesList";
+import { UserProfileAvatar } from "@/components/UserProfileAvatar";
 import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-import { FaUser } from "react-icons/fa";
 
 export const dynamic = "force-dynamic";
 
@@ -17,7 +15,6 @@ export default async function ProfilePage() {
   }
 
   const user = await getUser();
-
   if (!user) {
     redirect("/api/auth/login");
   }
@@ -29,60 +26,8 @@ export default async function ProfilePage() {
   return (
     <div className="min-h-screen flex flex-col py-6 p-6">
       <main className="flex-grow flex flex-col items-center">
-        <Card className="w-full max-w-6xl p-4 mb-8 bg-white shadow-lg bg-opacity-70">
-          <CardHeader className="flex items-center justify-center">
-            <Avatar className="w-40 h-40 mb-4">
-              <AvatarImage src={user.picture} alt={user.given_name} />
-              <AvatarFallback>
-                <FaUser className="text-6xl" />
-              </AvatarFallback>
-            </Avatar>
-            <h2 className="text-xl font-semibold">
-              {user.given_name} {user.family_name}
-            </h2>
-            <p className="text-gray-600">Email: {user.email}</p>
-          </CardHeader>
-        </Card>
-        <Card className="w-full max-w-6xl bg-white shadow-lg bg-opacity-70">
-          <CardHeader>
-            <CardTitle>Saved Color Palettes</CardTitle>
-          </CardHeader>
-          <CardContent>
-            {palettes.length > 0 ? (
-              palettes.map((palette) => (
-                <div
-                  key={palette.id}
-                  className="flex justify-between items-center p-2 border-b last:border-0"
-                >
-                  <div className="flex space-x-4">
-                    {palette.colors.map((color, index) => (
-                      <div key={index} className="flex items-center space-x-2">
-                        <div
-                          className="w-12 h-12 rounded"
-                          style={{ backgroundColor: color }}
-                        ></div>
-                        <span className="text-sm text-gray-700 w-[80px] text-center">
-                          {color}
-                        </span>
-                      </div>
-                    ))}
-                  </div>
-                  <span className="text-xl text-gray-700 font-medium min-w-[200px] text-center">
-                    {palette.name}
-                  </span>
-                  <form action={deletePalette}>
-                    <input type="hidden" name="id" value={palette.id} />
-                    <button type="submit" className="text-red-600 px-1">
-                      Delete
-                    </button>
-                  </form>
-                </div>
-              ))
-            ) : (
-              <p className="text-gray-600 text-center">No palettes found.</p>
-            )}
-          </CardContent>
-        </Card>
+        <UserProfileAvatar user={user} />
+        <UserPalettesList palettes={palettes} />
       </main>
     </div>
   );
